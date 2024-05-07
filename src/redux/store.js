6f7c61ba0606ab52597d95478185d082a6e1c5aa
anastasiyaaa0 <anastasiiiia98@gmail.com>
2024-05-07 22:47:12 +0300

    add  redux persist library

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,10 +1,24 @@
-import { configureStore } from '@reduxjs/toolkit';
+import { combineReducers, configureStore } from '@reduxjs/toolkit';
+import { persistStore, persistReducer } from 'redux-persist';
 import { contactsReducer } from './contactsSlice';
 import { filtersReducer } from './filtersSlice';
+import storage from 'redux-persist/lib/storage'; // defaults to localStorage for web
+
+const rootReducer = combineReducers({
+  contacts: contactsReducer,
+  filters: filtersReducer,
+});
+
+const persistConfig = {
+  key: 'root',
+  storage,
+  whitelist: ['contacts'],
+};
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
 
 export const store = configureStore({
-  reducer: {
-    contacts: contactsReducer,
-    filters: filtersReducer,
-  },
+  reducer: persistedReducer,
 });
+
+export const persistor = persistStore(store);
